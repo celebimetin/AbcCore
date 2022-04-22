86a8ef0ad8594833cfb0ec4f42e8f603f1c8de1d
Metin Çelebi <metincelebi5534@gmail.com>
2022-04-22 15:53:48 +0300

    Entity katmanının oluşturulması

---
+++ Abc.Entities/Concrete/Category.cs
@@ -0,0 +1,10 @@
+﻿using Abc.Core.Entities;
+
+namespace Abc.Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
